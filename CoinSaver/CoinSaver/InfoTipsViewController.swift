50c449c40425d6da5b145a05ecfdc2a095908a27
yayam15 <40362526+yayam15@users.noreply.github.com>
2021-08-05 14:51:28 -0400

    Finish Tips

---
+++ CoinSaver/CoinSaver/InfoTipsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InfoTipsViewController.swift
+//  CoinSaver
+//
+//  Created by Yazmyn Aguilar on 8/5/21.
+//
+
+import UIKit
+
+class InfoTipsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
